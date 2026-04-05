61d1e4e1140871f2496bb99b3db0380fdcd96f2f
alexander-g <3867427+alexander-g@users.noreply.github.com>
2026-04-05 11:38:51 +0000

    d3-heatmap refactoring

--- frontend/ui/d3-heatmap.tsx
+++ frontend/ui/d3-heatmap.tsx
@@ -1,20 +1,8 @@
 import { preact, Signal, signals, JSX } from "../dep.ts"
 
+import * as d3 from "d3";
 
-import { 
-    axisBottom,
-    axisLeft,
-    interpolateInferno,
-    pointer,
-    range,
-    select,
-    scaleBand,
-    scaleLinear,
-    scaleSequential,
-    ScaleBand,
-    zoom,
-    ZoomTransform,
-} from "d3";
+
 
 
 
@@ -36,7 +24,7 @@ export class D3Heatamp extends preact.Component<{
 
     on_click: (selected:number) => void,
 }> {
-    static override defaultProps = { /* cols: 50, rows: 30, */ width: 1400, height: 500 };
+    static override defaultProps = { width: 1400, height: 500 };
 
     svg_ref:     preact.RefObject<SVGSVGElement> = preact.createRef();
     root_ref:    preact.RefObject<SVGGElement> = preact.createRef();
@@ -46,10 +34,6 @@ export class D3Heatamp extends preact.Component<{
     svgimage_ref:  preact.RefObject<SVGImageElement> = preact.createRef();
 
     private margin = { top: 20, right: 5, bottom: 30, left: 60 };
-
-    override state = {
-        transform: new ZoomTransform( /*k = */ 1, /*x = */  0, /*y = */  0 )
-    };
 
 
 
@@ -101,78 +85,101 @@ export class D3Heatamp extends preact.Component<{
 
     #rowscols:{cols:number, rows:number}|null = null
 
+    /** The current number of rows and columns. Cached here to avoid recomputation */
+    private $rowscols:Signal<{cols:number, rows:number}|null> = new Signal(null)
+
     #_ = this.props.$data.subscribe( () => {
         this.#rowscols = this.#get_rows_cols()
-        this.update_axes()
-        this.update_heatmap()
+        this.$rowscols.value = this.#rowscols
     } )
 
 
+    /** The currently active D3 zoom/translation state */
+    private $zoom_transform:Signal<d3.ZoomTransform> = new Signal(
+        new d3.ZoomTransform( /*k = */ 1, /*x = */  0, /*y = */  0 )
+    )
+
     override componentDidMount(): void {
-        const z = zoom<SVGSVGElement, unknown>()
+        const zoom:d3.ZoomBehavior<SVGSVGElement, unknown> = 
+            d3.zoom<SVGSVGElement, unknown>()
             .scaleExtent([1, 25])
-            .on("zoom", (event) => {
-                this.setState({ transform: event.transform });
-            });
-        select(this.svg_ref.current!).call(z);
-
-        
-        const color = scaleSequential(interpolateInferno).domain([0, 1]);
-        this.update_heatmap();
-        this.update_axes();
-    }
-
-    override componentDidUpdate(): void {
-        this.update_axes();
+            .on("zoom", (event) => this.$zoom_transform.value = event.transform);
+        d3.select(this.svg_ref.current!).call(zoom);
     }
 
 
-    $transform_str: Signal<string> = new Signal('');
+    /** D3 ZoomTransform converted to a CSS transform string */
+    private $transform_str:Readonly<Signal<string>> = signals.computed( () => {
+        const t:d3.ZoomTransform = this.$zoom_transform.value;
 
-    update_axes() {
-
-        const { width, height } = this.props;
-        const colsrows = this.#rowscols
-        if(colsrows == null)
-            return;
-        const { cols, rows } = colsrows;
-
-        const w:number = width - this.margin.left - this.margin.right;
-        const h:number = height - this.margin.top - this.margin.bottom;
-
-        const t:ZoomTransform = this.state.transform
-        const sx = scaleLinear().domain([0, cols]).range([0, w])
-        const zx = (t && t.rescaleX)? t.rescaleX(sx) : sx;
-        const sy = scaleLinear().domain([0, rows]).range([0, h])
-        const zy = (t && t.rescaleY) ? t.rescaleY(sy) : sy;
-
-        select(this.xaxis_ref.current)
-            .call(
-                // @ts-ignore this is correct 
-                axisBottom(zx)
-                .tickValues(range(0, cols, 500))
-                // TODO: too many assumptions for this component
-                .tickFormat( t => strftime('%Y-%m-%d', new Date( this.props.$x_axis.value[Number(t)]! * 1000 ) )  )
-            );
-        select(this.yaxis_ref.current)
-            .call(
-                // @ts-ignore this is correct
-                axisLeft(zy)
-                .tickValues(range(0, rows, 5))
-                .tickFormat( d => Math.floor(Number(d)).toFixed(0)  )
-            );
-        
-        
         const k: number = t.k ?? 1;
         const tx:number = t.x ?? 0;
         const ty:number = t.y ?? 0;
         const transform_str = `translate(${tx},${ty}) scale(${k})`;
-        this.$transform_str.value = transform_str;
+        return transform_str;
+    })
 
+
+    update_axes = () => {
+        // NOTE: accessing $signals up here to make sure they are subscribed to
+        const t:d3.ZoomTransform = this.$zoom_transform.value
+        const x_axis:number[]    = this.props.$x_axis.value
+        const { width, height }  = this.props;
+        const colsrows:{cols:number, rows:number}|null = this.$rowscols.value
+        if(colsrows == null)
+            return;
+        const { cols, rows } = colsrows;
+
+        const w:number = width  - this.margin.left - this.margin.right;
+        const h:number = height - this.margin.top  - this.margin.bottom;
+
+        const zx:d3.ScaleLinear<number,number> = t.rescaleX(
+            d3.scaleLinear()
+            .domain([0, cols])
+            .range([0, w])
+        )
+        const zy:d3.ScaleLinear<number,number> = t.rescaleY(
+            d3.scaleLinear()
+            .domain([0, rows])
+            .range([0, h])
+        )
+
+
+        const step_size_x:number = Math.floor((zx.invert(w) - zx.invert(0)) / 10)
+
+        // index of first data column at plot position 0 (clipped)
+        const first_col_in_bounds:number = Math.max(zx.invert(0), 0)
+        const last_col_in_bounds:number = Math.min(zx.invert(w), cols)
+        
+
+        const d3_x_axis:d3.Axis<d3.NumberValue> = 
+            d3.axisBottom(zx)
+            .tickValues(
+                d3.range( 
+                    Math.ceil(first_col_in_bounds), 
+                    Math.floor(last_col_in_bounds), 
+                    step_size_x 
+                )
+            )
+            // TODO: too many assumptions for this component
+            .tickFormat( t => strftime('%Y-%m-%dT%H:%M:%S', new Date( x_axis[Number(t)]! * 1000 ) )  )
+        const d3_y_axis:d3.Axis<d3.NumberValue> = 
+            d3.axisLeft(zy)
+            .tickValues(d3.range(0, rows, 5))
+            .tickFormat( d => Math.floor(Number(d)).toFixed(0)
+        )
+
+        d3.select(this.xaxis_ref.current)
+            // @ts-ignore this is correct 
+            .call(d3_x_axis);
+        d3.select(this.yaxis_ref.current)
+            // @ts-ignore this is correct 
+            .call(d3_y_axis);
     }
+    #_2 = signals.effect( this.update_axes )
 
 
-
+    /** Compute the current number of rows and columns from the data input */
     #get_rows_cols(): {cols:number, rows:number}|null {
         const data:DataItem[] = this.props.$data.value;
         if(data.length == 0)
@@ -191,16 +198,15 @@ export class D3Heatamp extends preact.Component<{
         }
     }
 
-    async update_heatmap() {
-        const colsrows = this.#rowscols
+    /** Render the current data input onto the <image> element. */
+    update_heatmap = async () => {
+        // NOTE: $data.value is up here to make sure its subscribed
+        const data:DataItem[] = this.props.$data.value;
+        const colsrows:{cols:number, rows:number}|null = this.$rowscols.value
         if(colsrows == null)
             return;
         const { cols, rows } = colsrows;
-
-
         const [h,w] = [rows, cols];
-        console.log('w/h', w,h)
-       
 
         const canvas = new OffscreenCanvas(w, h);
         const ctx:OffscreenRenderingContext = canvas.getContext('2d')!;
@@ -211,7 +217,7 @@ export class D3Heatamp extends preact.Component<{
 
         const imdata:ImageData = ctx.getImageData(0,0,w,h);
         const buffer:Uint8ClampedArray = imdata.data; // w*h*4
-        for(const item of this.props.$data.value) {
+        for(const item of data) {
             const index:number = item.y * w * 4 + item.x * 4;
             buffer[index + 0] = item.value * 255; 
             buffer[index + 1] = item.value * 255;
@@ -224,13 +230,14 @@ export class D3Heatamp extends preact.Component<{
         const blob = await canvas.convertToBlob({ type: 'image/png' });
         const f = new File([blob], "file.png", { type: blob.type });
         
-        const svgimage = this.svgimage_ref.current!
+        const svgimage:SVGImageElement = this.svgimage_ref.current!
         svgimage.href.baseVal = URL.createObjectURL(f)
-
     }
+    #_1 = signals.effect( () => { this.update_heatmap() } )
+
 
     svgimage_onclick:preact.MouseEventHandler<SVGImageElement> = (event) => {
-        const [mx, my] = pointer(event, this.svgimage_ref.current);
+        const [mx, my] = d3.pointer(event, this.svgimage_ref.current);
         const { width, height } = this.props;
         const w:number = width - this.margin.left - this.margin.right;
         const h:number = height - this.margin.top - this.margin.bottom;
